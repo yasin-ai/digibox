135386b818565aafd130853a8f82ce436ceec83a
awesome-yasin <yasinthegodofcrypto@gmail.com>
2023-03-03 17:32:32 +0530

    yas

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const loginRoutes = require('./routes/route')
 const signupRoutes = require('./routes/route')
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({extended: true}));
