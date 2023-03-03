074f832e7c185138ad7201da3c360d756b044588
awesome-yasin <yasinthegodofcrypto@gmail.com>
2023-03-03 17:20:16 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express')
 const app = express()
 const router = express.Router();
-
+const axios = require('axios')
 router.get('/', (req, res, next) =>{
     res.render('main')
 });
@@ -13,4 +13,19 @@ router.get('/login', (req, res) =>{
 router.get('/signup', (req, res) =>{
     res.render('signup')
 })
+router.get('/candidate/:username', async (req, res) => {
+    const { username } = req.query;
+    try {
+      const response = await axios.get(`https://digirecruitserver.azurewebsites.net/candidate/details?username=${username}`);
+      const candidateData = response.data;
+      console.log(candidateData)
+      res.json(candidateData);
+      res.render('candidate', {candidateData})
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Failed to fetch candidate data' });
+    }
+  });
+  
+  
 module.exports =  router;
